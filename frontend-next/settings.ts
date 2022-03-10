981d96ad34219f0ff7ae9b96a073c40c677036a3
Julien Blanchon <julien@blanchon.cc>
2022-03-11 00:05:22 +0100

    Suppr settings.ts

--- frontend-next/settings.ts
+++
@@ -1,3 +0,0 @@
-export const parcelContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const itemsContractAddress = "0x9f1ac54BEF0DD2f6f3462EA0fa94fC62300d3a8e"
-export const resContractAddress = "0xCafac3dD18aC6c6e92c921884f9E4176737C052c"
