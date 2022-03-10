01769ca9a51dfe77ca3af3119795c18a0478bdba
Julien Blanchon <julien@blanchon.cc>
2022-03-10 23:40:46 +0100

    gas limit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -47,7 +47,7 @@ const config: HardhatUserConfig = {
     },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      gas: 500000,
+      gas: 50000,
       accounts: {
         mnemonic: `${test_mnemonic}`,
       }
