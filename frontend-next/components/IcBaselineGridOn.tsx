a10b579b5c1a3a96ae7b3e831aa29f67c4978c19
nohehf <nohe.hinniger.foray@gmail.com>
2022-03-11 21:35:01 +0100

    nav ok

---
+++ frontend-next/components/IcBaselineGridOn.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function IcBaselineGridOn(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM8 20H4v-4h4v4zm0-6H4v-4h4v4zm0-6H4V4h4v4zm6 12h-4v-4h4v4zm0-6h-4v-4h4v4zm0-6h-4V4h4v4zm6 12h-4v-4h4v4zm0-6h-4v-4h4v4zm0-6h-4V4h4v4z"></path></svg>
+  )
+}
+export default IcBaselineGridOn
--- frontend-next/components/Nav.tsx
+++ frontend-next/components/Nav.tsx
@@ -1,19 +1,35 @@
 import Link from "next/link";
 import { useAccount } from "wagmi";
 import AuthButton from "./AuthButton";
+import IcBaselineGridOn from "./IcBaselineGridOn";
+import IcRoundHome from "./IcRoundHome";
 import Player from "./Player";
 
 interface Props {}
 const Nav: React.FC<Props> = ({}) => {
   const [{ data, error, loading }, disconnect] = useAccount();
   return (
-    <nav className="flex h-full p-0.5 px-2 justify-between w-full text-white bg-blue-900 bg-opacity-50">
-      <Link href={"/"}>Grid</Link>
+    <nav className="flex h-full p-0.5 px-2 justify-between items-center w-full text-white bg-blue-900 bg-opacity-50">
+      <div className="flex text-xl w-[35%]">
+        <div className="mr-3">
+          <Link href={"/"}>
+            <IcRoundHome />
+          </Link>
+        </div>
+        <div>
+          <Link href={"/"}>
+            <IcBaselineGridOn />
+          </Link>
+        </div>
+      </div>
+
       <h1 className="text-3xl font-unifraktur">
-        Parcel <span className="font-sans text-xl align-top">™️</span>
+        Parcels <span className="font-sans text-xl align-top">™️</span>
       </h1>
-      {/* <Link href={"/wallet"}>Wallet</Link> */}
-      {data?.address ? <Player /> : <AuthButton />}
+      <div className="w-[35%]">
+        {/* <Link href={"/wallet"}>Wallet</Link> */}
+        {data?.address ? <Player /> : <AuthButton />}
+      </div>
     </nav>
   );
 };
